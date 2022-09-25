6823b6e2f0109bb7705b17959dc63e0e777c1ebf
Danyil-Repenko <danyil.oficial@gmail.com>
2022-09-25 23:08:48 +0300

    Перше завдання

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Stats } from 'components/Profile/Stats/Stats.jsx';
+
+export const Profile = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt="User avatar" />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <Stats stats={stats} />
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
